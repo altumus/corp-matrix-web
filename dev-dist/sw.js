b3c2c779e6fba1c326e80d87f983db1a945d1a51
Максим Бухарин <altumus@Mac-mini-Maksim.local>
2026-05-04 01:06:27 +0300

    feat: version 1.1.9

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -83,7 +83,7 @@ define(['./workbox-c5fd805d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.62n2kl95k14"
+    "revision": "0.e4eu21nitf8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
